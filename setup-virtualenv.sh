e43773347303913bf07eb8f3c9c8ae8ceca02b9a
Sean Laurent <organicveggie@gmail.com>
2022-10-16 00:24:05 -0700

    Use lxd on GitHub

--- setup-virtualenv.sh
+++ setup-virtualenv.sh
@@ -2,19 +2,10 @@
 
 echo virtualenv venv
 virtualenv venv
-source venv/bin/activate
+. venv/bin/activate
 
 echo pip install --upgrade pip
 pip install --upgrade pip
 
-echo pip install "ansible"
-pip install "ansible"
-
-echo pip install "molecule"
-pip install "molecule"
-
-echo pip install "ansible-lint"
-pip install "ansible-lint"
-
-echo pip install "molecule[docker]"
-pip install "molecule[docker]"
+echo pip install ansible molecule "ansible-lint" "molecule[docker]" "molecule-lxd" "molecule[lxd]"
+pip install ansible molecule "ansible-lint" "molecule[docker]" "molecule-lxd" "molecule[lxd]"
